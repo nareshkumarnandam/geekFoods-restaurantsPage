db880ea9aa31f99076bd6179c3d51b63a8b33329
naresh <nareshcena666@gmail.com>
2024-03-13 23:51:41 +0530

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -54,25 +54,27 @@ const Home = () => {
                         key={item._id.$oid}
                         style={{
                         border: "1px solid black",
-                        padding: "0.5rem",
-                        borderRadius: "1rem",
+                        padding: "0",
+                        borderRadius: "10px",
                         width: "25rem",
+                        border:"none",
+
                         }}
                     >
-                        <div style={{display:'flex', justifyContent:'space-evenly', alignItems:'center'}}>
-                            <h5>{item.name}</h5>
+                        <div style={{display:'flex', justifyContent:'flex-start', alignItems:'center', padding:' 0 20px', gap:'30px'}}>
+                            <h4>{item.name}</h4>
                             <p>Rating : {item.rating}</p>
                         </div>
-                        <div>
+                        <div style={{display:'flex', justifyContent:'flex-start', alignItems:'center', padding:' 0 20px', gap:'30px'}}>
                             <p>{item.address}</p>
                         </div>
-                        <div>
+                        <div style={{display:'flex', justifyContent:'flex-start', alignItems:'center', padding:' 0 20px', gap:'20px'}}>
                             <p>{item.outcode}</p>
                             <p>{item.postcode}</p>
                         </div>
-                        <div>
+                        <div style={{display:'flex',flexDirection:'column', justifyContent:'center', alignItems:'flex-start',borderBottomRightRadius:'10px', borderBottomLeftRadius:'10px', padding:'10px 20px', gap:'0px', backgroundColor:'lightgray', width:'90%', margin:'0'}}>
                             <p>{item.type_of_food}</p>
-                            <a href={item.URL}>Visit menu</a>
+                            <a href={item.URL} style={{textDecoration:'none', color:'#3B82F6', cursor:'pointer'}}>Visit menu</a>
                         </div>
                         
                         
